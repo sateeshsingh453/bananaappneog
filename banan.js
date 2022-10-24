9b42a597add77ac8c50a88505e1022602d2944e2
Sateesh Singh <47327595+sateeshsingh453@users.noreply.github.com>
2022-10-24 15:54:47 +0530

    Input output processing done

---
+++ banan.js
@@ -0,0 +1,15 @@
+// console.log("ajkajka");
+// var username = prompt("Give Your username");
+// alert("This script works " +username);
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+// outputDiv.innerText=" Naman "
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+   // console.log("input",txtInput.value)
+   outputDiv.innerText=" ahdskjskjkjk " +  textInput.value
+
+});
+
+
